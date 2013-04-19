c12963bae5d905f71114ce3beefedc2b3a83192d
Jake Wharton <jw@squareup.com>
2013-04-19 13:59:21 -0700

    Forbid target instances from recycling bitmaps.

---
+++ picasso/src/test/java/com/squareup/picasso/TargetRequestTest.java
@@ -0,0 +1,29 @@
+package com.squareup.picasso;
+
+import android.graphics.Bitmap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.fail;
+
+@RunWith(PicassoTestRunner.class)
+public class TargetRequestTest {
+  @Test public void recyclingInSuccessThrowsException() {
+    Target recycler = new Target() {
+      @Override public void onSuccess(Bitmap bitmap) {
+        bitmap.recycle();
+      }
+
+      @Override public void onError() {
+        throw new AssertionError();
+      }
+    };
+    TargetRequest tr = new TargetRequest(null, null, 0, recycler, null, null, null, 0, null);
+    tr.result = Bitmap.createBitmap(10, 10, null);
+    try {
+      tr.complete();
+      fail();
+    } catch (IllegalStateException expected) {
+    }
+  }
+}
