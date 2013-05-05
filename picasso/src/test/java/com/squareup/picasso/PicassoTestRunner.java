610a9245c9c222a341ce6071e30f2e18f558a59d
Jake Wharton <jw@squareup.com>
2013-05-05 00:05:41 -0700

    Fix robolectric test runner override.

--- picasso/src/test/java/com/squareup/picasso/PicassoTestRunner.java
+++ picasso/src/test/java/com/squareup/picasso/PicassoTestRunner.java
@@ -1,16 +1,16 @@
 package com.squareup.picasso;
 
-import java.io.File;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.AndroidManifest;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.res.FsFile;
 
 public class PicassoTestRunner extends RobolectricTestRunner {
   public PicassoTestRunner(Class<?> testClass) throws InitializationError {
     super(testClass);
   }
 
-  @Override protected AndroidManifest createAppManifest(File baseDir) {
+  @Override protected AndroidManifest createAppManifest(FsFile manifestFile) {
     return null;
   }
 }
