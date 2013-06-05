1daf427880b550d3fd74986a4677aecd723f4db8
Al Sutton <al@funkyandroid.com>
2013-06-05 20:12:04 +0100

    Fixed formatting

--- picasso/src/main/java/com/squareup/picasso/OkHttpLoader.java
+++ picasso/src/main/java/com/squareup/picasso/OkHttpLoader.java
@@ -17,48 +17,48 @@ public class OkHttpLoader implements Loader {
 
   private final OkHttpClient client;
 
-      /**
-       * Create new loader that uses OkHttp. This will install an image cache into your application
-       * cache directory.
-       */
-      public OkHttpLoader(final Context context) {
-        this(Utils.createDefaultCacheDir(context));
-      }
+  /**
+   * Create new loader that uses OkHttp. This will install an image cache into your application
+   * cache directory.
+   */
+  public OkHttpLoader(final Context context) {
+    this(Utils.createDefaultCacheDir(context));
+  }
 
-    /**
-     * Create new loader that uses OkHttp. This will install an image cache into your application
-     * cache directory.
-     *
-     * @param cacheDir The directory in which the cache should be stored
-     */
-    public OkHttpLoader(final File cacheDir) {
-        this(cacheDir, Utils.calculateDiskCacheSize(cacheDir));
-    }
+  /**
+   * Create new loader that uses OkHttp. This will install an image cache into your application
+   * cache directory.
+   *
+   * @param cacheDir The directory in which the cache should be stored
+   */
+  public OkHttpLoader(final File cacheDir) {
+    this(cacheDir, Utils.calculateDiskCacheSize(cacheDir));
+  }
 
-    /**
-     * Create new loader that uses OkHttp. This will install an image cache into your application
-     * cache directory.
-     *
-     * @param maxSize The size limit for the cache.
-     */
-    public OkHttpLoader(final Context context, final int maxSize) {
-        this(Utils.createDefaultCacheDir(context), maxSize);
-    }
+  /**
+   * Create new loader that uses OkHttp. This will install an image cache into your application
+   * cache directory.
+   *
+   * @param maxSize The size limit for the cache.
+   */
+  public OkHttpLoader(final Context context, final int maxSize) {
+    this(Utils.createDefaultCacheDir(context), maxSize);
+  }
 
-    /**
-     * Create new loader that uses OkHttp. This will install an image cache into your application
-     * cache directory.
-     *
-     * @param cacheDir The directory in which the cache should be stored
-     * @param maxSize The size limit for the cache.
-     */
-    public OkHttpLoader(final File cacheDir, final int maxSize) {
-        this(new OkHttpClient());
-        try {
-            client.setResponseCache(new HttpResponseCache(cacheDir, maxSize));
-        } catch (IOException ignored) {
-        }
+  /**
+   * Create new loader that uses OkHttp. This will install an image cache into your application
+   * cache directory.
+   *
+   * @param cacheDir The directory in which the cache should be stored
+   * @param maxSize The size limit for the cache.
+   */
+  public OkHttpLoader(final File cacheDir, final int maxSize) {
+    this(new OkHttpClient());
+    try {
+      client.setResponseCache(new HttpResponseCache(cacheDir, maxSize));
+    } catch (IOException ignored) {
     }
+  }
 
   /**
    * Create a new loader that uses the specified OkHttp instance. A response cache will not be
