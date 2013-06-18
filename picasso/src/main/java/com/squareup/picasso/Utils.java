867a28b3f0686ed359879bf0c971d3103fb94c43
Mahram Z. Foadi <mahram.foadi@corp.hookt.com>
2013-06-17 19:24:49 -0700

    Missing contact should return null immediately.
    Closes #91.

--- picasso/src/main/java/com/squareup/picasso/Utils.java
+++ picasso/src/main/java/com/squareup/picasso/Utils.java
@@ -236,6 +236,9 @@ final class Utils {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
       if (uri.toString().startsWith(ContactsContract.Contacts.CONTENT_LOOKUP_URI.toString())) {
         uri = ContactsContract.Contacts.lookupContact(contentResolver, uri);
+        if (uri == null) {
+          return null;
+        }
       }
       return openContactPhotoInputStream(contentResolver, uri);
     } else {
