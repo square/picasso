a3b9d065cc150463288aa59d45a717794ea1cd0f
Alexey Zakharov <alexey.v.zaharov@gmail.com>
2013-06-21 14:08:05 +0600

    fix mvn warnings

--- picasso/src/main/java/com/squareup/picasso/Picasso.java
+++ picasso/src/main/java/com/squareup/picasso/Picasso.java
@@ -47,7 +47,7 @@ public class Picasso {
    */
   private static final Object DECODE_LOCK = new Object();
 
-    /** Callbacks for Picasso events. */
+  /** Callbacks for Picasso events. */
   public interface Listener {
     /**
      * Invoked when an image has failed to load. This is useful for reporting image failures to a
@@ -240,11 +240,17 @@ public class Picasso {
       try {
         bitmap = loadFromType(request);
       } catch (OutOfMemoryError e) {
-         if (request.options != null && request.options.targetHeight > DECODE_MINIMAL_SIZE && request.options.targetWidth > DECODE_MINIMAL_SIZE) {
-            request.options.targetHeight = request.options.targetHeight / 2;
-            request.options.targetWidth = request.options.targetWidth / 2;
-            request.options.inJustDecodeBounds = true;
+        PicassoBitmapOptions options = request.options;
+        if (options != null) {
+          int targetHeight = options.targetHeight;
+          int targetWidth = options.targetWidth;
+          if (targetHeight > DECODE_MINIMAL_SIZE && targetWidth > DECODE_MINIMAL_SIZE) {
+            options.targetHeight = targetHeight / 2;
+            options.targetWidth = targetWidth / 2;
+            options.inJustDecodeBounds = true;
+
             return resolveRequest(request);
+          }
          } else {
           throw new IOException("Failed to decode request: " + request, e);
         }
