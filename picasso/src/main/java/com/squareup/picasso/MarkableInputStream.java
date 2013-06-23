88f496bbe6ffcd987ca92e71c1e6e5ce94524dec
Jamie Hewland <jhewland@gmail.com>
2013-06-23 13:28:09 +0200

    Fix namespace collision

--- picasso/src/main/java/com/squareup/picasso/MarkableInputStream.java
+++ picasso/src/main/java/com/squareup/picasso/MarkableInputStream.java
@@ -110,7 +110,7 @@ final class MarkableInputStream extends InputStream {
   @Override public int read(byte[] buffer, int offset, int length) throws IOException {
     int count = in.read(buffer, offset, length);
     if (count != -1) {
-      offset += count;
+      this.offset += count;
     }
     return count;
   }
