3c9b8160bea4ad1059366912312bf75b18c1f206
Jorge Juan Barroso Carmona <jbarroso@tuenti.com>
2013-09-13 12:20:00 +0200

    First commit

---
+++ picasso/src/main/java/com/squareup/picasso/SoftReferencesCache.java
@@ -0,0 +1,9 @@
+package com.squareup.picasso;
+
+/**
+ * Copyright (c) Tuenti Technologies. All rights reserved.
+ *
+ * @author "Jorge J. Barroso" <jbarroso@tuenti.com>
+ */
+public class SoftReferencesCache {
+}
