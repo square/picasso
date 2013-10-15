7442d9bd8878314c6d8e9d8464b1ed7ef6ab8aeb
Jake Wharton <jw@squareup.com>
2013-10-15 00:22:36 -0700

    Correct for skia decoding problem during underflow.
    Adapted from http://b.android.com/6066#c12

--- picasso/src/main/java/com/squareup/picasso/MarkableInputStream.java
+++ picasso/src/main/java/com/squareup/picasso/MarkableInputStream.java
@@ -102,6 +102,13 @@ final class MarkableInputStream extends InputStream {
   private void skip(long current, long target) throws IOException {
     while (current < target) {
       long skipped = in.skip(target - current);
+      if (skipped == 0) {
+        if (read() == -1) {
+          break; // EOF
+        } else {
+          skipped = 1;
+        }
+      }
       current += skipped;
     }
   }
