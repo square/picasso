2d87b893779eac3ce4d73479e91fa4441fd433fa
Tomasz Rozbicki <tomrozb+code@gmail.com>
2013-11-05 01:19:38 +0100

    Fix javadoc parenthesis

--- picasso/src/main/java/com/squareup/picasso/RequestCreator.java
+++ picasso/src/main/java/com/squareup/picasso/RequestCreator.java
@@ -116,7 +116,7 @@ public class RequestCreator {
    * Attempt to resize the image to fit exactly into the target {@link ImageView}'s bounds. This
    * will result in delayed execution of the request until the {@link ImageView} has been measured.
    * <p/>
-   * <em>Note:</em> This method works only when your target is an {@link ImageView).
+   * <em>Note:</em> This method works only when your target is an {@link ImageView}.
    */
   public RequestCreator fit() {
     deferred = true;
