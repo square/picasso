5cecac6f7d86fe313f39a2ae5fb0f8609863fd10
Mahram Z. Foadi <mahram.foadi@corp.hookt.com>
2014-01-25 20:18:32 -0800

    refactored the contacts photo retrieval to allow actual photo uris as well. The previous implementeation only worked with contact and lookup uris.

--- picasso/src/main/java/com/squareup/picasso/ContactsPhotoBitmapHunter.java
+++ picasso/src/main/java/com/squareup/picasso/ContactsPhotoBitmapHunter.java
@@ -33,8 +33,35 @@ import static android.provider.ContactsContract.Contacts.openContactPhotoInputSt
 import static com.squareup.picasso.Picasso.LoadedFrom.DISK;
 
 class ContactsPhotoBitmapHunter extends BitmapHunter {
-  final Context context;
+  /**
+   a lookup uri (e.g. content://com.android.contacts/contacts/lookup/3570i61d948d30808e537 )
+  */
+  private static final int ID_LOOKUP = 1;
+  /**
+   a contact thumbnail uri (e.g. content://com.android.contacts/contacts/38/photo)
+   */
+  private static final int ID_THUMBNAIL = 2;
+  /**
+   a contact uri (e.g. content://com.android.contacts/contacts/38)
+   */
+  private static final int ID_CONTACT = 3;
+  /**
+   a contact display photo (high resolution) uri
+   (e.g. content://com.android.contacts/display_photo/5)
+   */
+  private static final int ID_DISPLAY_PHOTO = 4;
+
   static final UriMatcher matcher;
+
+  static {
+    matcher = new UriMatcher(UriMatcher.NO_MATCH);
+    matcher.addURI(ContactsContract.AUTHORITY, "contacts/lookup/*", ID_LOOKUP);
+    matcher.addURI(ContactsContract.AUTHORITY, "contacts/#/photo", ID_THUMBNAIL);
+    matcher.addURI(ContactsContract.AUTHORITY, "contacts/#", ID_CONTACT);
+    matcher.addURI(ContactsContract.AUTHORITY, "display_photo/#", ID_DISPLAY_PHOTO);
+  }
+
+  final Context context;
 
   ContactsPhotoBitmapHunter(Context context, Picasso picasso, Dispatcher dispatcher, Cache cache,
       Stats stats, Action action) {
@@ -61,22 +88,23 @@ class ContactsPhotoBitmapHunter extends BitmapHunter {
     ContentResolver contentResolver = context.getContentResolver();
     Uri uri = getData().uri;
     switch (matcher.match(uri)) {
-        case ID_LOOKUP:
-          uri = ContactsContract.Contacts.lookupContact(contentResolver, uri);
-          if (null == uri) {
-            return null;
-          }
-        case ID_CONTACT:
-          if (SDK_INT < ICE_CREAM_SANDWICH) {
-            return openContactPhotoInputStream(contentResolver, uri);
-          } else {
-            return ContactPhotoStreamIcs.get(contentResolver, uri);
-          }
-        case ID_THUMBNAIL:
-        case ID_DISPLAY_PHOTO:
-          return contentResolver.openInputStream(uri);
-        default:
+      case ID_LOOKUP:
+        uri = ContactsContract.Contacts.lookupContact(contentResolver, uri);
+        if (null == uri) {
           return null;
+        }
+        // Resolved the uri to a contact uri, intentionally fall through to process the resolved uri
+      case ID_CONTACT:
+        if (SDK_INT < ICE_CREAM_SANDWICH) {
+          return openContactPhotoInputStream(contentResolver, uri);
+        } else {
+          return ContactPhotoStreamIcs.get(contentResolver, uri);
+        }
+      case ID_THUMBNAIL:
+      case ID_DISPLAY_PHOTO:
+        return contentResolver.openInputStream(uri);
+      default:
+        throw new IllegalStateException ("Invalid uri: " + uri);
     }
   }
 
@@ -104,31 +132,5 @@ class ContactsPhotoBitmapHunter extends BitmapHunter {
     static InputStream get(ContentResolver contentResolver, Uri uri) {
       return openContactPhotoInputStream(contentResolver, uri, true);
     }
-  }
-
-  /**
-   a lookup uri (e.g. content://com.android.contacts/contacts/lookup/3570i61d948d30808e537 )
-   */
-  private static final int ID_LOOKUP = 1;
-  /**
-   a contact thumbnail uri (e.g. content://com.android.contacts/contacts/38/photo)
-   */
-  private static final int ID_THUMBNAIL = 2;
-  /**
-   a contact uri (e.g. content://com.android.contacts/contacts/38)
-   */
-  private static final int ID_CONTACT = 3;
-  /**
-   a contact display photo (high resolution) uri
-   (e.g. content://com.android.contacts/display_photo/5)
-   */
-  private static final int ID_DISPLAY_PHOTO = 4;
-
-  static {
-      matcher = new UriMatcher(UriMatcher.NO_MATCH);
-      matcher.addURI(ContactsContract.AUTHORITY, "contacts/lookup/*", ID_LOOKUP);
-      matcher.addURI(ContactsContract.AUTHORITY, "contacts/#/photo", ID_THUMBNAIL);
-      matcher.addURI(ContactsContract.AUTHORITY, "contacts/#", ID_CONTACT);
-      matcher.addURI(ContactsContract.AUTHORITY, "display_photo/#", ID_DISPLAY_PHOTO);
   }
 }
