078115e8e262edfe91ea62e4fa127df9eacec34d
Jake Wharton <jw@squareup.com>
2014-02-07 15:07:21 -0500

    Bracket.

--- picasso/src/main/java/com/squareup/picasso/RequestCreator.java
+++ picasso/src/main/java/com/squareup/picasso/RequestCreator.java
@@ -180,7 +180,7 @@ public class RequestCreator {
    * Attempt to decode the image using the specified config.
    * <p>
    * Note: This value may be ignored by {@link BitmapFactory}. See
-   * {@link BitmapFactory.Options#inPreferredConfig its documentation for more details.
+   * {@link BitmapFactory.Options#inPreferredConfig its documentation} for more details.
    */
   public RequestCreator config(Bitmap.Config config) {
     data.config(config);
