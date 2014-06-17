square/picasso

commit 18c84271169729eb71207244245a3260480c72c7

Make PicassoDrawable extend BitmapDrawable instead.

picasso/src/main/java/com/squareup/picasso/PicassoDrawable.java
@@ -16,7 +16,6 @@
 package com.squareup.picasso;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
@@ -33,10 +32,9 @@ import android.widget.ImageView;
 import static android.graphics.Color.WHITE;
 import static com.squareup.picasso.Picasso.LoadedFrom.MEMORY;
 
-final class PicassoDrawable extends Drawable {
+final class PicassoDrawable extends BitmapDrawable {
   // Only accessed from main thread.
   private static final Paint DEBUG_PAINT = new Paint();
-
   private static final float FADE_DURATION = 200f; //ms
 
   /**
@@ -50,7 +48,7 @@ final class PicassoDrawable extends Drawable {
       ((AnimationDrawable) placeholder).stop();
     }
     PicassoDrawable drawable =
-        new PicassoDrawable(context, placeholder, bitmap, loadedFrom, noFade, debugging);
+        new PicassoDrawable(context, bitmap, placeholder, loadedFrom, noFade, debugging);
     target.setImageDrawable(drawable);
   }
 
@@ -72,7 +70,6 @@ final class PicassoDrawable extends Drawable {
   private final boolean debugging;
   private final float density;
   private final Picasso.LoadedFrom loadedFrom;
-  final BitmapDrawable image;
 
   Drawable placeholder;
 
@@ -80,16 +77,14 @@ final class PicassoDrawable extends Drawable {
   boolean animating;
   int alpha = 0xFF;
 
-  PicassoDrawable(Context context, Drawable placeholder, Bitmap bitmap,
+  PicassoDrawable(Context context, Bitmap bitmap, Drawable placeholder,
       Picasso.LoadedFrom loadedFrom, boolean noFade, boolean debugging) {
-    Resources res = context.getResources();
+    super(context.getResources(), bitmap);
 
     this.debugging = debugging;
-    this.density = res.getDisplayMetrics().density;
+    this.density = context.getResources().getDisplayMetrics().density;
 
     this.loadedFrom = loadedFrom;
-
-    this.image = new BitmapDrawable(res, bitmap);
 
     boolean fade = loadedFrom != MEMORY && !noFade;
     if (fade) {
@@ -101,23 +96,22 @@ final class PicassoDrawable extends Drawable {
 
   @Override public void draw(Canvas canvas) {
     if (!animating) {
-      image.draw(canvas);
+      super.draw(canvas);
     } else {
       float normalized = (SystemClock.uptimeMillis() - startTimeMillis) / FADE_DURATION;
       if (normalized >= 1f) {
         animating = false;
         placeholder = null;
-        image.draw(canvas);
+        super.draw(canvas);
       } else {
         if (placeholder != null) {
           placeholder.draw(canvas);
         }
 
         int partialAlpha = (int) (alpha * normalized);
-        image.setAlpha(partialAlpha);
-        image.draw(canvas);
-        image.setAlpha(alpha);
-        invalidateSelf();
+        setAlpha(partialAlpha);
+        super.draw(canvas);
+        setAlpha(alpha);
       }
     }
 
@@ -126,40 +120,25 @@ final class PicassoDrawable extends Drawable {
     }
   }
 
-  @Override public int getIntrinsicWidth() {
-    return image.getIntrinsicWidth();
-  }
-
-  @Override public int getIntrinsicHeight() {
-    return image.getIntrinsicHeight();
-  }
-
   @Override public void setAlpha(int alpha) {
-    this.alpha = alpha;
     if (placeholder != null) {
       placeholder.setAlpha(alpha);
     }
-    image.setAlpha(alpha);
+    super.setAlpha(alpha);
   }
 
   @Override public void setColorFilter(ColorFilter cf) {
     if (placeholder != null) {
       placeholder.setColorFilter(cf);
     }
-    image.setColorFilter(cf);
-  }
-
-  @Override public int getOpacity() {
-    return image.getOpacity();
+    super.setColorFilter(cf);
   }
 
   @Override protected void onBoundsChange(Rect bounds) {
-    super.onBoundsChange(bounds);
-
-    image.setBounds(bounds);
     if (placeholder != null) {
       placeholder.setBounds(bounds);
     }
+    super.onBoundsChange(bounds);
   }
 
   private void drawDebugIndicator(Canvas canvas) {

picasso/src/test/java/com/squareup/picasso/PicassoDrawableTest.java
@@ -16,7 +16,6 @@
 package com.squareup.picasso;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import org.junit.Test;
@@ -25,7 +24,6 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static android.graphics.Bitmap.Config.ARGB_8888;
 import static android.graphics.Color.RED;
 import static com.squareup.picasso.Picasso.LoadedFrom.DISK;
 import static com.squareup.picasso.Picasso.LoadedFrom.MEMORY;
@@ -39,22 +37,22 @@ public class PicassoDrawableTest {
   private final Drawable placeholder = new ColorDrawable(RED);
 
   @Test public void createWithNoPlaceholderAnimation() {
-    PicassoDrawable pd = new PicassoDrawable(context, null, BITMAP_1, DISK, false, false);
-    assertThat(pd.image.getBitmap()).isSameAs(BITMAP_1);
+    PicassoDrawable pd = new PicassoDrawable(context, BITMAP_1, null, DISK, false, false);
+    assertThat(pd.getBitmap()).isSameAs(BITMAP_1);
     assertThat(pd.placeholder).isNull();
     assertThat(pd.animating).isTrue();
   }
 
   @Test public void createWithPlaceholderAnimation() {
-    PicassoDrawable pd = new PicassoDrawable(context, placeholder, BITMAP_1, DISK, false, false);
-    assertThat(pd.image.getBitmap()).isSameAs(BITMAP_1);
+    PicassoDrawable pd = new PicassoDrawable(context, BITMAP_1, placeholder, DISK, false, false);
+    assertThat(pd.getBitmap()).isSameAs(BITMAP_1);
     assertThat(pd.placeholder).isSameAs(placeholder);
     assertThat(pd.animating).isTrue();
   }
 
   @Test public void createWithBitmapCacheHit() {
-    PicassoDrawable pd = new PicassoDrawable(context, placeholder, BITMAP_1, MEMORY, false, false);
-    assertThat(pd.image.getBitmap()).isSameAs(BITMAP_1);
+    PicassoDrawable pd = new PicassoDrawable(context, BITMAP_1, placeholder, MEMORY, false, false);
+    assertThat(pd.getBitmap()).isSameAs(BITMAP_1);
     assertThat(pd.placeholder).isNull();
     assertThat(pd.animating).isFalse();
   }

commit f536b2506c91226a9ad9a89f57645499ec01fb82

Restore keeping track of desired drawable alpha
- https://github.com/square/picasso/issues/25
- 18c8427 broke alpha support on PicassoDrawable

picasso/src/main/java/com/squareup/picasso/PicassoDrawable.java
@@ -110,9 +110,9 @@ final class PicassoDrawable extends BitmapDrawable {
         }
 
         int partialAlpha = (int) (alpha * normalized);
-        setAlpha(partialAlpha);
+        super.setAlpha(partialAlpha);
         super.draw(canvas);
-        setAlpha(alpha);
+        super.setAlpha(alpha);
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
           invalidateSelf();
         }
@@ -125,6 +125,7 @@ final class PicassoDrawable extends BitmapDrawable {
   }
 
   @Override public void setAlpha(int alpha) {
+    this.alpha = alpha;
     if (placeholder != null) {
       placeholder.setAlpha(alpha);
     }