f3bcb95f663feb47df45736829c8e028275789f3
Jake Wharton <jw@squareup.com>
2014-10-28 22:40:23 -0700

    Disambiguate generic type parameter on inner class.

--- picasso/src/main/java/com/squareup/picasso/Action.java
+++ picasso/src/main/java/com/squareup/picasso/Action.java
@@ -22,10 +22,10 @@ import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 
 abstract class Action<T> {
-  static class RequestWeakReference<T> extends WeakReference<T> {
+  static class RequestWeakReference<M> extends WeakReference<M> {
     final Action action;
 
-    public RequestWeakReference(Action action, T referent, ReferenceQueue<? super T> q) {
+    public RequestWeakReference(Action action, M referent, ReferenceQueue<? super M> q) {
       super(referent, q);
       this.action = action;
     }
