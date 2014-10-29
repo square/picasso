0574a1ed7cd8bdc0cadb510004d42758cb6c288d
dimitris <dimitris@squareup.com>
2014-10-29 16:32:53 -0700

    Create and use Object target for Fetch actions

--- picasso/src/main/java/com/squareup/picasso/FetchAction.java
+++ picasso/src/main/java/com/squareup/picasso/FetchAction.java
@@ -17,14 +17,22 @@ package com.squareup.picasso;
 
 import android.graphics.Bitmap;
 
-class FetchAction extends Action<Void> {
+class FetchAction extends Action<Object> {
+
+  private final Object target;
+
   FetchAction(Picasso picasso, Request data, boolean skipCache, String key, Object tag) {
     super(picasso, null, data, skipCache, false, 0, null, key, tag);
+    this.target = new Object();
   }
 
   @Override void complete(Bitmap result, Picasso.LoadedFrom from) {
   }
 
   @Override public void error() {
+  }
+
+  @Override Object getTarget() {
+    return target;
   }
 }
