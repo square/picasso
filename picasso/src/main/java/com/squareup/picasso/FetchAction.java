bc2830449e591c3f7a73e4849da423a88b29583e
PeggsDev <peggsdev@gmail.com>
2014-12-18 15:54:59 +0000

    Added call back to fetch function so we can be notified of when images have been successfully processed, Usefull for progress/load bar

--- picasso/src/main/java/com/squareup/picasso/FetchAction.java
+++ picasso/src/main/java/com/squareup/picasso/FetchAction.java
@@ -19,12 +19,20 @@ import android.graphics.Bitmap;
 
 class FetchAction extends Action<Object> {
 
-  private final Object target;
+    private final Object target;
+    private Callback callback;
 
   FetchAction(Picasso picasso, Request data, int memoryPolicy, int networkPolicy, Object tag,
       String key) {
     super(picasso, null, data, memoryPolicy, networkPolicy, 0, null, key, tag, false);
     this.target = new Object();
+  }
+
+  FetchAction(Picasso picasso, Request data, int memoryPolicy, int networkPolicy, Object tag,
+      String key, Callback callback) {
+    super(picasso, null, data, memoryPolicy, networkPolicy, 0, null, key, tag, false);
+    this.target = new Object();
+    this.callback = callback;
   }
 
   @Override void complete(Bitmap result, Picasso.LoadedFrom from) {
