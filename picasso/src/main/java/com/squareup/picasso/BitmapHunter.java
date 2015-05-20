beb6bebea836deb6d2e6aabf9497b1fd1551d38d
JohnWowUs <thenegus@gmail.com>
2015-05-20 15:22:06 +0100

    Generalized dimension recalculation on rotation fixes

--- picasso/src/main/java/com/squareup/picasso/BitmapHunter.java
+++ picasso/src/main/java/com/squareup/picasso/BitmapHunter.java
@@ -504,16 +504,69 @@ class BitmapHunter implements Runnable {
 
     Matrix matrix = new Matrix();
 
-    if (data.needsMatrixTransform()) {
+    if (data.needsMatrixTransform() || exifOrientation != 0) {
       int targetWidth = data.targetWidth;
       int targetHeight = data.targetHeight;
 
       float targetRotation = data.rotationDegrees;
       if (targetRotation != 0) {
+        double cosR = Math.cos(Math.toRadians(targetRotation));
+        double sinR = Math.sin(Math.toRadians(targetRotation));
         if (data.hasRotationPivot) {
           matrix.setRotate(targetRotation, data.rotationPivotX, data.rotationPivotY);
+          // Recalculate dimensions after rotation around pivot point
+          double x1T = data.rotationPivotX * (1.0 - cosR) + (data.rotationPivotY * sinR);
+          double y1T = data.rotationPivotY * (1.0 - cosR) - (data.rotationPivotX * sinR);
+          double x2T = x1T + (data.targetWidth * cosR);
+          double y2T = y1T + (data.targetWidth * sinR);
+          double x3T = x1T + (data.targetWidth * cosR) - (data.targetHeight * sinR);
+          double y3T = y1T + (data.targetWidth * sinR) + (data.targetHeight * cosR);
+          double x4T = x1T - (data.targetHeight * sinR);
+          double y4T = y1T + (data.targetHeight * cosR);
+
+          double maxX = Math.max(x4T, Math.max(x3T, Math.max(x1T, x2T)));
+          double minX = Math.min(x4T, Math.min(x3T, Math.min(x1T, x2T)));
+          double maxY = Math.max(y4T, Math.max(y3T, Math.max(y1T, y2T)));
+          double minY = Math.min(y4T, Math.min(y3T, Math.min(y1T, y2T)));
+          targetWidth = (int) Math.floor(maxX - minX);
+          targetHeight  = (int) Math.floor(maxY - minY);
         } else {
           matrix.setRotate(targetRotation);
+          // Recalculate dimensions after rotation (around origin)
+          double x1T = 0.0;
+          double y1T = 0.0;
+          double x2T = (data.targetWidth * cosR);
+          double y2T = (data.targetWidth * sinR);
+          double x3T = (data.targetWidth * cosR) - (data.targetHeight * sinR);
+          double y3T = (data.targetWidth * sinR) + (data.targetHeight * cosR);
+          double x4T = -(data.targetHeight * sinR);
+          double y4T = (data.targetHeight * cosR);
+
+          double maxX = Math.max(x4T, Math.max(x3T, Math.max(x1T, x2T)));
+          double minX = Math.min(x4T, Math.min(x3T, Math.min(x1T, x2T)));
+          double maxY = Math.max(y4T, Math.max(y3T, Math.max(y1T, y2T)));
+          double minY = Math.min(y4T, Math.min(y3T, Math.min(y1T, y2T)));
+          targetWidth = (int) Math.floor(maxX - minX);
+          targetHeight  = (int) Math.floor(maxY - minY);
+        }
+      }
+
+      // EXIf interpretation should be done before cropping in case the dimensions need to
+      // be recalculated
+      if (exifOrientation != 0) {
+        int exifRotation = getExifRotation(exifOrientation);
+        int exifTranslation = getExifTranslation(exifOrientation);
+        if (exifRotation != 0) {
+          matrix.preRotate(exifRotation);
+          if (exifRotation == 90 || exifRotation == 270) {
+             // Recalculate dimensions after exif rotation
+             int tmpHeight = targetHeight;
+             targetHeight = targetWidth;
+             targetWidth = tmpHeight;
+          }
+        }
+        if (exifTranslation != 1) {
+          matrix.postScale(exifTranslation, 1);
         }
       }
 
@@ -566,14 +619,6 @@ class BitmapHunter implements Runnable {
         if (shouldResize(onlyScaleDown, inWidth, inHeight, targetWidth, targetHeight)) {
           matrix.preScale(sx, sy);
         }
-      }
-    }
-
-    if (exifOrientation != 0) {
-      matrix.preRotate(getExifRotation(exifOrientation));
-      int exifTranslation = getExifTranslation(exifOrientation);
-      if (exifTranslation != 1) {
-        matrix.postScale(exifTranslation, 1);
       }
     }
 
