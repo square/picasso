da01a7eb984d3b2ea8e237d3d9bbaedba3050ec5
Andrew Williams <awilliams@nextfaze.com>
2015-07-16 08:38:32 +0930

    fix for exif orientation detection on samsung galaxy s4 (issue #1083)

--- picasso/src/main/java/com/squareup/picasso/MediaStoreRequestHandler.java
+++ picasso/src/main/java/com/squareup/picasso/MediaStoreRequestHandler.java
@@ -20,17 +20,18 @@ import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.provider.MediaStore;
+
 import java.io.IOException;
 
 import static android.content.ContentResolver.SCHEME_CONTENT;
-import static android.content.ContentUris.parseId;
 import static android.provider.MediaStore.Images;
-import static android.provider.MediaStore.Video;
 import static android.provider.MediaStore.Images.Thumbnails.FULL_SCREEN_KIND;
 import static android.provider.MediaStore.Images.Thumbnails.MICRO_KIND;
 import static android.provider.MediaStore.Images.Thumbnails.MINI_KIND;
+import static android.provider.MediaStore.Video;
 import static com.squareup.picasso.MediaStoreRequestHandler.PicassoKind.FULL;
 import static com.squareup.picasso.MediaStoreRequestHandler.PicassoKind.MICRO;
 import static com.squareup.picasso.MediaStoreRequestHandler.PicassoKind.MINI;
@@ -38,8 +39,14 @@ import static com.squareup.picasso.Picasso.LoadedFrom.DISK;
 
 class MediaStoreRequestHandler extends ContentStreamRequestHandler {
   private static final String[] CONTENT_ORIENTATION = new String[] {
-      Images.ImageColumns.ORIENTATION
+          Images.ImageColumns.ORIENTATION,
   };
+  private static final String[] CONTENT_DATA = new String[] {
+          Images.Media.DATA
+  };
+
+  // KitKat media uris
+  private static final String MEDIA_DOCUMENTS_PROVIDER = "com.android.providers.media.documents";
 
   MediaStoreRequestHandler(Context context) {
     super(context);
@@ -47,19 +54,22 @@ class MediaStoreRequestHandler extends ContentStreamRequestHandler {
 
   @Override public boolean canHandleRequest(Request data) {
     final Uri uri = data.uri;
-    return (SCHEME_CONTENT.equals(uri.getScheme())
-            && MediaStore.AUTHORITY.equals(uri.getAuthority()));
+    final String authority = uri.getAuthority();
+
+    return SCHEME_CONTENT.equals(uri.getScheme())
+            && (MediaStore.AUTHORITY.equals(authority)
+            || MEDIA_DOCUMENTS_PROVIDER.equals(authority));
   }
 
   @Override public Result load(Request request, int networkPolicy) throws IOException {
     ContentResolver contentResolver = context.getContentResolver();
     int exifOrientation = getExifOrientation(contentResolver, request.uri);
 
-    String mimeType = contentResolver.getType(request.uri);
-    boolean isVideo = mimeType != null && mimeType.startsWith("video/");
+    boolean isVideo = isVideo(contentResolver, request.uri);
 
     if (request.hasSize()) {
       PicassoKind picassoKind = getPicassoKind(request.targetWidth, request.targetHeight);
+
       if (!isVideo && picassoKind == FULL) {
         return new Result(null, getInputStream(request), DISK, exifOrientation);
       }
@@ -80,8 +90,8 @@ class MediaStoreRequestHandler extends ContentStreamRequestHandler {
         int kind = (picassoKind == FULL) ? Video.Thumbnails.MINI_KIND : picassoKind.androidKind;
         bitmap = Video.Thumbnails.getThumbnail(contentResolver, id, kind, options);
       } else {
-        bitmap =
-            Images.Thumbnails.getThumbnail(contentResolver, id, picassoKind.androidKind, options);
+        bitmap = Images.Thumbnails.getThumbnail(contentResolver, id,
+                picassoKind.androidKind, options);
       }
 
       if (bitmap != null) {
@@ -90,6 +100,19 @@ class MediaStoreRequestHandler extends ContentStreamRequestHandler {
     }
 
     return new Result(null, getInputStream(request), DISK, exifOrientation);
+  }
+
+  // example uris:
+  //  content://media/external/images/media/42
+  //  content://com.android.providers.media.documents/document/image:42  (KitKat)
+  static long parseId(Uri uri) {
+    // handles image:xx uris
+    return Long.parseLong(uri.getLastPathSegment().replaceAll("\\D", ""));
+  }
+
+  static boolean isVideo(ContentResolver contentResolver, Uri uri) {
+    String mimeType = contentResolver.getType(uri);
+    return mimeType != null && mimeType.startsWith("video/");
   }
 
   static PicassoKind getPicassoKind(int targetWidth, int targetHeight) {
@@ -101,22 +124,77 @@ class MediaStoreRequestHandler extends ContentStreamRequestHandler {
     return FULL;
   }
 
-  static int getExifOrientation(ContentResolver contentResolver, Uri uri) {
-    Cursor cursor = null;
-    try {
-      cursor = contentResolver.query(uri, CONTENT_ORIENTATION, null, null, null);
-      if (cursor == null || !cursor.moveToFirst()) {
-        return 0;
+  int getExifOrientation(ContentResolver contentResolver, Uri uri) {
+    int orientation = getExifOrientationFromContentResolver(contentResolver, uri);
+    if (orientation == ExifInterface.ORIENTATION_UNDEFINED) {
+      // attempt to get exif orientation from file
+      orientation = getExifOrientationFromFile(contentResolver, uri);
+    }
+    return orientation;
+  }
+
+  int getExifOrientationFromContentResolver(ContentResolver contentResolver, Uri uri) {
+    String orientationStr = queryContentResolver(contentResolver, uri, CONTENT_ORIENTATION, null,
+            new ContentResolverCursorCallback() {
+              @Override
+              public String handleCursor(Cursor cursor) {
+                return cursor.getString(0);
+              }
+            });
+    return orientationStr == null ? 0 : Integer.parseInt(orientationStr);
+  }
+
+  int getExifOrientationFromFile(ContentResolver contentResolver, Uri uri) {
+    int orientation = 0;
+    String path = getMediaPath(contentResolver, uri);
+    if (path != null) {
+      try {
+        ExifInterface exif = new ExifInterface(path);
+        orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL);
+      } catch (IOException e) {
+        // thrown on ExifInterface error
+        Utils.log(MediaStoreRequestHandler.class.getSimpleName(), Utils.VERB_ERRORED, "");
       }
-      return cursor.getInt(0);
-    } catch (RuntimeException ignored) {
-      // If the orientation column doesn't exist, assume no rotation.
-      return 0;
+    }
+    return orientation;
+  }
+
+  String getMediaPath(ContentResolver contentResolver, Uri uri) {
+    boolean isVideo = isVideo(contentResolver, uri);
+    Uri baseUri = isVideo ? Video.Media.EXTERNAL_CONTENT_URI : Images.Media.EXTERNAL_CONTENT_URI;
+    long id = parseId(uri);
+
+    return queryContentResolver(contentResolver, baseUri, CONTENT_DATA, "_id=" + id,
+            new ContentResolverCursorCallback() {
+              @Override
+              public String handleCursor(Cursor cursor) {
+                return cursor.getString(0);
+              }
+            });
+  }
+
+  String queryContentResolver(ContentResolver contentResolver, Uri uri, String[] projection,
+                            String selection, ContentResolverCursorCallback callback) {
+    Cursor cursor = null;
+
+    try {
+      cursor = contentResolver.query(uri, projection, selection, null, null);
+      if (cursor != null && cursor.moveToFirst() && callback != null) {
+        return callback.handleCursor(cursor);
+      }
+    } catch (Exception e) {
+      Utils.log(MediaStoreRequestHandler.class.getSimpleName(), Utils.VERB_ERRORED, "");
     } finally {
       if (cursor != null) {
         cursor.close();
       }
     }
+    return null;
+  }
+
+  interface ContentResolverCursorCallback {
+    String handleCursor(Cursor cursor);
   }
 
   enum PicassoKind {
