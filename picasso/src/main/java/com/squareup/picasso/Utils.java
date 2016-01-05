c2b317bf51bcea7ef0f72cf0bf42d94b92cd45d6
Jake Wharton <jw@squareup.com>
2016-01-05 17:15:04 -0500

    Add support for OkHttp 3.

--- picasso/src/main/java/com/squareup/picasso/Utils.java
+++ picasso/src/main/java/com/squareup/picasso/Utils.java
@@ -247,11 +247,16 @@ final class Utils {
 
   static Downloader createDefaultDownloader(Context context) {
     if (SDK_INT >= GINGERBREAD) {
-        try {
-          Class.forName("com.squareup.okhttp.OkHttpClient");
-          return OkHttpLoaderCreator.create(context);
-        } catch (ClassNotFoundException ignored) {
-        }
+      try {
+        Class.forName("okhttp3.OkHttpClient");
+        return OkHttp3DownloaderCreator.create(context);
+      } catch (ClassNotFoundException ignored) {
+      }
+      try {
+        Class.forName("com.squareup.okhttp.OkHttpClient");
+        return OkHttpDownloaderCreator.create(context);
+      } catch (ClassNotFoundException ignored) {
+      }
     }
     return new UrlConnectionDownloader(context);
   }
@@ -425,9 +430,15 @@ final class Utils {
     }
   }
 
-  private static class OkHttpLoaderCreator {
+  private static class OkHttpDownloaderCreator {
     static Downloader create(Context context) {
       return new OkHttpDownloader(context);
+    }
+  }
+
+  private static class OkHttp3DownloaderCreator {
+    static Downloader create(Context context) {
+      return new OkHttp3Downloader(context);
     }
   }
 }
