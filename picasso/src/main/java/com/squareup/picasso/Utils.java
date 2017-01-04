381471354b1b52bd29b674c93cfa57982c9b68b3
Jake Wharton <jw@squareup.com>
2017-01-04 18:06:32 -0500

    Remove old large heap querying abstraction.
    This is no longer needed because the minSdk is 14.

--- picasso/src/main/java/com/squareup/picasso/Utils.java
+++ picasso/src/main/java/com/squareup/picasso/Utils.java
@@ -41,7 +41,6 @@ import static android.content.Context.ACTIVITY_SERVICE;
 import static android.content.pm.ApplicationInfo.FLAG_LARGE_HEAP;
 import static android.os.Build.VERSION.SDK_INT;
 import static android.os.Build.VERSION_CODES.GINGERBREAD;
-import static android.os.Build.VERSION_CODES.HONEYCOMB;
 import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
 import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR2;
 import static android.os.Build.VERSION_CODES.KITKAT;
@@ -291,10 +290,7 @@ final class Utils {
   static int calculateMemoryCacheSize(Context context) {
     ActivityManager am = getService(context, ACTIVITY_SERVICE);
     boolean largeHeap = (context.getApplicationInfo().flags & FLAG_LARGE_HEAP) != 0;
-    int memoryClass = am.getMemoryClass();
-    if (largeHeap && SDK_INT >= HONEYCOMB) {
-      memoryClass = ActivityManagerHoneycomb.getLargeMemoryClass(am);
-    }
+    int memoryClass = largeHeap ? am.getLargeMemoryClass() : am.getMemoryClass();
     // Target ~15% of the available heap.
     return (int) (1024L * 1024L * memoryClass / 7);
   }
@@ -403,13 +399,6 @@ final class Utils {
       }
     };
     handler.sendMessageDelayed(handler.obtainMessage(), THREAD_LEAK_CLEANING_MS);
-  }
-
-  @TargetApi(HONEYCOMB)
-  private static class ActivityManagerHoneycomb {
-    static int getLargeMemoryClass(ActivityManager activityManager) {
-      return activityManager.getLargeMemoryClass();
-    }
   }
 
   static class PicassoThreadFactory implements ThreadFactory {
