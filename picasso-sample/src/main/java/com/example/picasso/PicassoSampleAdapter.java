bc63f01749c7bd35969101c86077730bc2ec068f
John Rodriguez <jrod@squareup.com>
2018-03-05 03:22:38 -0500

    Fix Notifications to use channels on O+

--- picasso-sample/src/main/java/com/example/picasso/PicassoSampleAdapter.java
+++ picasso-sample/src/main/java/com/example/picasso/PicassoSampleAdapter.java
@@ -2,10 +2,12 @@ package com.example.picasso;
 
 import android.app.Activity;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +21,7 @@ import java.util.Random;
 final class PicassoSampleAdapter extends BaseAdapter {
 
   private static final int NOTIFICATION_ID = 666;
+  private static final String CHANNEL_ID = "channel-id";
 
   enum Sample {
     GRID_VIEW("Image Grid View", SampleGridViewActivity.class),
@@ -32,15 +35,25 @@ final class PicassoSampleAdapter extends BaseAdapter {
 
         Intent intent = new Intent(activity, SampleGridViewActivity.class);
 
-        NotificationCompat.Builder builder =
-            new NotificationCompat.Builder(activity).setSmallIcon(R.drawable.icon)
+        Notification notification =
+            new NotificationCompat.Builder(activity, CHANNEL_ID)
+                .setSmallIcon(R.drawable.icon)
                 .setContentIntent(PendingIntent.getActivity(activity, -1, intent, 0))
-                .setContent(remoteViews);
-
-        Notification notification = builder.getNotification();
+                .setContent(remoteViews)
+                .setAutoCancel(true)
+                .setChannelId(CHANNEL_ID)
+                .build();
 
         NotificationManager notificationManager =
             (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          NotificationChannel channel =
+              new NotificationChannel(CHANNEL_ID, "Picasso Notification Channel",
+                  NotificationManager.IMPORTANCE_DEFAULT);
+          notificationManager.createNotificationChannel(channel);
+        }
+
         notificationManager.notify(NOTIFICATION_ID, notification);
 
         // Now load an image for this notification.
